1577c894074e49a132f572643d2bcdd7debb207b
Joris <joris.tve@gmail.com>
2025-11-25 20:41:07 +0100

    bugfix + windows compatibility

--- helper_functions.py
+++ helper_functions.py
@@ -2,6 +2,7 @@ import re
 import os
 import json
 import convert
+from pathlib import PurePosixPath
 
 def allowed_file(filename, allowed_extensions):
     return '.' in filename and os.path.splitext(filename)[1].lower() in allowed_extensions
@@ -9,7 +10,7 @@ def allowed_file(filename, allowed_extensions):
 def clean_filename(filename):
     filename = filename.strip()
     filename = filename.replace("\'", "`").replace("\"", "`") 
-    filename = re.sub(r'[^\w\s.`-]', '', filename)
+    filename = re.sub(r'[^A-Za-z0-9 ._\-`]', '', filename)
     return filename
 
 def load_watched(watched_file):
@@ -24,10 +25,12 @@ def save_watched(watched_movies, watched_file):
     with open(watched_file, "w") as fp:
         json.dump(watched_movies, fp, indent=4)
 
-def get_subtitles(subpath, media_dir, verbose = False):
+def get_subtitles(subpath, media_dir, verbose = False) -> tuple[str, bool]:
     # check if .vtt subtitles exist, if not check for .srt and convert to .vtt
     
     root = os.path.splitext(subpath)[0]
+    root = str(PurePosixPath(root)) # fix backslashes on Windows machines
+    
     subtitle_url = f"/media/{root}.vtt"
     subtitle_path = os.path.join(media_dir, f"{root}.vtt")
 
@@ -36,30 +39,32 @@ def get_subtitles(subpath, media_dir, verbose = False):
     
     has_subtitles = os.path.exists(subtitle_path)  
 
-    if has_subtitles and verbose:
-        print("vtt subtitles found")
+    if has_subtitles:
+        if verbose:
+            print(".vtt subtitles found")
+            
         return subtitle_url, True
         
-    else:
-        srt_path = os.path.join(media_dir, f"{root}.srt")
+    srt_path = os.path.join(media_dir, f"{root}.srt")
 
+    if verbose:
+        print("checking", srt_path)
+
+    if os.path.exists(srt_path):
         if verbose:
-            print("checking", srt_path)
-    
-        if os.path.exists(srt_path):
-            if verbose:
-                print("srt subtitles found")
-            try:
-                convert.to_utf_8(srt_path, verbose = verbose)
-                convert.srt_to_vtt(srt_path, subtitle_path, verbose = verbose)
-                return subtitle_url, True
-            except Exception as e:
-                if verbose:
-                    print(".srt to .vtt conversion failed:", e)
-                    print("subpath:", subpath)
-                return "", False
+            print(".srt subtitles found")
             
-        else:
+        try:
+            convert.to_utf_8(srt_path, verbose = verbose)
+            convert.srt_to_vtt(srt_path, subtitle_path, verbose = verbose)
+            return subtitle_url, True
+            
+        except Exception as e:
             if verbose:
-                print("no subtitles found")
-            return "", False 
+                print(".srt to .vtt conversion failed:", e)
+                print("subpath:", subpath)
+            return "", False
+        
+    if verbose:
+        print("no subtitles found")
+    return "", False 
